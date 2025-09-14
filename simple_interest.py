ef5efd20f38853684596a7e7ade9a853eecdf70f
Synk01 <90985840+Synk01@users.noreply.github.com>
2025-09-14 21:07:03 +0100

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,10 @@
+# Define the variables for the simple interest calculation
+principal = 1000
+rate = 0.05
+time = 3
+
+#formula: I = P * R * T
+interest = principal * rate * time
+
+# Print the final result
+print("The simple interest is:", interest)
