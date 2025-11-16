67e0f9082b92fae360330225eeb3fc958c8ee4b0
Synk01 <90985840+Synk01@users.noreply.github.com>
2025-11-16 23:52:47 +0100

    push the 2nd task

---
+++ control-flow/mmatch_case_calculator.py
@@ -0,0 +1,26 @@
+# first, for the user to input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# to choose an operation
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case '+':
+        result = num1 + num2
+        print(f"The result is: {result}")
+    case '-':
+        result = num1 - num2
+        print(f"The result is: {result}")
+    case '*':
+        result = num1 * num2
+        print(f"The result is: {result}")
+    case '/':
+        # Handle division by zero
+        if num2 == 0:
+            print("Error! Division by zero is not allowed.")
+        else:
+            result = num1 / num2
+            print(f"The result is: {result}")
+    case _:
+        print("Invalid operation. Please choose from +, -, *, /.")
